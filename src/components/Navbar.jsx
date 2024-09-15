109618672d677b017beb1b33066ca62e5d1f466b
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-15 20:40:27 +0300

    added classnames to the header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { appleImg, searchImg, bagImg } from "../utils";
 
 const Navbar = () => {
   return (
-    <header>
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
       <nav>
         <img src={appleImg} alt="Apple" width={14} height={18} />
 
