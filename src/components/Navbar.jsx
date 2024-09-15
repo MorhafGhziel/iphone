0b2006cd48048fcaacbe2ef16efcbd82896d2ed1
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-15 20:33:46 +0300

    created an array and mapped over it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,12 @@ const Navbar = () => {
     <header>
       <nav>
         <img src={appleImg} alt="Apple" width={14} height={18} />
+
+        <div>
+          {["Phones", "Macbooks", "Tablets"].map((nav, i) => (
+            <div key={i}>{nav}</div>
+          ))}
+        </div>
       </nav>
     </header>
   );
