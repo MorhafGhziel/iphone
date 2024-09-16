0c18510bce3a6d5227de51967ed822c28354d199
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-16 16:42:36 +0300

    added opacity animation to the p

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,17 @@ import { useGSAP } from "@gsap/react";
 
 const Hero = () => {
   useGSAP(() => {
-    gsap.to();
+    gsap.to("#hero", {
+      opacity: 1,
+      delay: 1.5,
+    });
   }, []);
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p className="hero-title">iPhone 15 Pro</p>
+        <p className="hero-title" id="hero">
+          iPhone 15 Pro
+        </p>
       </div>
     </section>
   );
