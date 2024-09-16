fec2a3c5701d18b23ba460e66b1b77b6e3227358
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-16 18:35:02 +0300

    styled the p

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -56,7 +56,7 @@ const Hero = () => {
         <a href="#highLights" className="btn">
           Buy
         </a>
-        <p>From $199/month or $999</p>
+        <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
   );
