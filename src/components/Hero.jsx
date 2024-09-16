8473f8179391b9bb52c407bdb62555198dbf2d35
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-16 15:42:30 +0300

    styled the div

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,9 @@
 const Hero = () => {
-  return <section className="w-full nav-height bg-black relative"></section>;
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col"></div>
+    </section>
+  );
 };
 
 export default Hero;
