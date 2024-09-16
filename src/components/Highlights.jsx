8b257f1903ddcad9f1dbd1ca3fa8b85356c034bd
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-16 19:14:06 +0300

    imported the rightImg

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,6 +1,6 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { watchImg } from "../utils";
+import { watchImg, rightImg } from "../utils";
 
 const Highlights = () => {
   useGSAP(() => {
