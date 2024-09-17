de8e87fe35b25b6ffb30555b1d4b6b3707957e37
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-17 17:09:52 +0300

    ned edited from test to list

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -4,8 +4,8 @@ const VideoCarousel = () => {
   return (
     <>
       <div className="flex items-center">
-        {hightlightsSlides.map((test) => (
-          <div key={test.id} id="slider" className="sm:pr-20 pr-10">
+        {hightlightsSlides.map((list) => (
+          <div key={list.id} id="slider" className="sm:pr-20 pr-10">
             <div className="video-carousel_container">
               <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
                 <video>
