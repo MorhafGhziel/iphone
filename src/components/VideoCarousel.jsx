7c8e697181cbd3d770067422a3cb8f25bb6b1b74
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-17 16:51:04 +0300

    starting the map and imported react

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,9 +1,12 @@
 import highlightSlides from "../constants";
+import React from "react";
 
 const VideoCarousel = () => {
   return (
     <>
-      <div className="flex items-center">{highlightSlides}</div>
+      <div className="flex items-center">{highlightSlides.map((list, i) => (
+
+      ))}</div>
     </>
   );
 };
